eb78c678f1a74d0192238d4244ba5083322e7c55
andreafrasson95 <andreafrasson1995@gmail.com>
2021-06-01 19:03:16 +0200

    added

---
+++ src/java/servlet/AbstractDatabaseServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
+
+public class AbstractDatabaseServlet extends HttpServlet {
+
+
+    //Connection pool to the  database
+    private DataSource ds;
+
+    public void init(ServletConfig config) throws ServletException{
+
+        //JNDI lookup context
+        InitialContext cxt;
+
+        try{
+            cxt = new InitialContext();
+            ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/poll-webapp");
+        }catch(NamingException e){
+            ds = null;
+
+            throw new ServletException(
+                    String.format("Impossible to access the connection pool to the database: %s",e.getMessage()));
+        }
+    }
+
+    public void destroy(){ ds = null;}
+
+    protected final DataSource getDataSource(){ return ds;}
+}
